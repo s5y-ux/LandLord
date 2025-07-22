cc86262fa1b4baf8e9498622546f27249ba172a1
s5y-ux <jadenraymond16@gmail.com>
2025-07-21 21:09:05 -0700

    Updated the plugin

--- src/main/java/net/ddns/vcccd/UpdateChecker.java
+++ src/main/java/net/ddns/vcccd/UpdateChecker.java
@@ -13,7 +13,7 @@ import org.bukkit.event.player.PlayerJoinEvent;
 
 public class UpdateChecker implements Listener {
 	
-	private String GameVersion = "0.1.1";
+	private String GameVersion = "0.1.2";
 	private String Version = constructData();
 
     //Used to construct the JSON data for the rest of the class
